f5c743c08458cf1a38c5d04cf978de04d39fbfe9
Victor Azizi <victor@lipsum.eu>
2021-03-09 12:30:05 +0100

    DALES MPI, for all standard modules

--- src/modsubgrid.f90
+++ src/modsubgrid.f90
@@ -100,8 +100,7 @@ contains
 
   subroutine subgridnamelist
     use modglobal, only : ifnamopt,fname_options,checknamelisterror
-    use mpi
-    use modmpi,    only : myid, comm3d, mpierr, my_real, mpi_logical
+    use modmpi,    only : myid, comm3d, mpierr, mpi_logical, D_MPI_BCAST
 
     implicit none
 
@@ -118,6 +117,7 @@ contains
       close(ifnamopt)
     end if
 
+<<<<<<< HEAD
     call MPI_BCAST(ldelta     ,1,MPI_LOGICAL,0,comm3d,mpierr)
     call MPI_BCAST(lmason     ,1,MPI_LOGICAL,0,comm3d,mpierr)
     call MPI_BCAST(nmason     ,1,MY_REAL    ,0,comm3d,mpierr)
@@ -129,6 +129,20 @@ contains
     call MPI_BCAST(Prandtl    ,1,MY_REAL   ,0,comm3d,mpierr)
     call MPI_BCAST(sgs_surface_fix ,1,MPI_LOGICAL   ,0,comm3d,mpierr)
     call MPI_BCAST(ch1        ,1,MY_REAL   ,0,comm3d,mpierr)
+=======
+    call D_MPI_BCAST(ldelta          ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(lmason          ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(lsmagorinsky    ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(lanisotrop      ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(sgs_surface_fix ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(nmason          ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(cs              ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(cf              ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(cn              ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(Rigc            ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(Prandtl         ,1, 0,comm3d,mpierr)
+    call D_MPI_BCAST(ch1             ,1, 0,comm3d,mpierr)
+>>>>>>> 895c46a (DALES MPI, for all standard modules)
 
   end subroutine subgridnamelist
 
